ef921a00a22f49b1f1f8af4fe0129090c8d93619
Concavebark <concavebarkplaysgames@yahoo.com>
2019-09-17 17:01:27 -0400

    Add files via upload

--- pecWindowBaseBuild02BACKUP.py
+++ pecWindowBaseBuild02BACKUP.py
@@ -1,69 +1,46 @@
-import tkinter
-from tkinter import scrolledtext, Menu, messagebox, ttk
-from tkinter import *
+import tkinter as tk
+from tkinter import scrolledtext, Menu, messagebox, ttk, showinfo
 import email, smtplib, ssl, sys
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-def popup():
-    window = tkinter.Toplevel()
-    window.wm_title("Input Creditials")
-    window.maxsize(350,100)
+class popupWindow(tkinter.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        top=self.top=Toplevel(master)
 
-    def pushCreds():
-        global setPass
-        global setUser
-        setPass = passEntry.get()
-        setUser = userEntry.get()
-        window.destroy()
+        button = tkinter.Button(self,text="Something",command=None)
 
-    userLabel = tkinter.Label(window, text="Input Username: ")
-    userLabel.grid(row=0, column=0)
+        button.pack()
 
-    userEntry = tkinter.Entry(window, width=40)
-    userEntry.grid(row=0, column=1)
-
-    passLabel = tkinter.Label(window, text="Input Password: ")
-    passLabel.grid(row=1, column=0)
-
-    passEntry = tkinter.Entry(window, width=40)
-    passEntry.grid(row=1, column=1)
-
-    button = tkinter.Button(window, text="Enter", command=pushCreds)
-    button.grid(row=2, column=0)
-
-
+    def cleanup(self):
+        self.value = self.master.inputField.get()
+        self.destroy()
 
 class mainWindow(tkinter.Frame):
     def __init__(self, master):
         super().__init__(master)
 
-        def build(): #instead of "Send" button going here, go straight to popup?
+        def build():
+            senderEmail = "pythontestingconcave@gmail.com"
             recipiantEmail = recipiantInput.get()
             emailSubject = subjectInput.get()
             emailText = emailInput.get('1.0','end-1c')
-            try:
-                password = setPass
-                senderEmail = setUser
 
-                message = MIMEMultipart('alternative')
+            message = MIMEMultipart('alternative')
 
-                message["Subject"] = str(emailSubject)
-                message["From"] = str(senderEmail) # CHANGE FOR INPUT/USER
-                message["To"] = str(recipiantEmail)
+            message["Subject"] = str(emailSubject)
+            message["From"] = str(senderEmail) # CHANGE FOR INPUT/USER
+            message["To"] = str(recipiantEmail)
 
-                messageText = emailText
-                plainText = MIMEText(messageText, 'plain')
+            messageText = emailText
+            plainText = MIMEText(messageText, 'plain') #The issue doesn't seem to be with text
 
-                message.attach(plainText)
+            message.attach(plainText)
 
-                send(recipiantEmail, senderEmail, message, password)
-            except NameError:
-                popup()
+            send(recipiantEmail, senderEmail, message, "Mason_123")
 
-
-
-        def send(recipiantEmail, senderEmail, message, password):
+        def send(recipiantEmail, senderEmail, message, password): #REQUIRE PASSWORD PRIOR, popup
             context = ssl.create_default_context()
 
             with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
@@ -89,28 +66,27 @@ class mainWindow(tkinter.Frame):
 
         subButton = tkinter.Button(self,text="Send",command=build)
         subButton.grid(column=0, row=3)
-
-        #button = tkinter.Button(self,text="popup",command=popup)
-        #button.grid(column=1,row=3)
+        button = tkinter.Button(self,text="popup",command=self.popUp)
+        button.grid(column=1,row=3)
 
         def clearFields():
             recipiantInput.delete('0', 'end')
             subjectInput.delete('0','end')
             emailInput.delete('1.0','end-1c')
-            #Have this clear the active user.
 
         menubar = Menu(self.master)
         self.master.config(menu=menubar)
 
         fileMenu = Menu(menubar)
-        userMenu = Menu(menubar)
         fileMenu.add_command(label="New Email", command=clearFields)
         fileMenu.add_command(label="Exit", command=self.master.destroy)
         menubar.add_cascade(label="File", menu=fileMenu)
-        userMenu.add_command(label="pythontestingconcave@gmail.com", command=None)
-        menubar.add_cascade(label="Users", menu=userMenu)
 
         self.pack()
+
+    def popUp(self):
+            self.w=popupWindow(self.master)
+            self.master.wait_window(self.w.top)
 
 
 root=Tk()
--- text.py
+++ text.py
@@ -4,32 +4,32 @@ import sys
 class popupWindow(object):
     def __init__(self,master):
         top=self.top=Toplevel(master)
-        self.l=Label(top,text="Hello World")
-        self.l.pack()
-        self.e=Entry(top)
-        self.e.pack()
-        self.b=Button(top,text='Ok',command=self.cleanup)
-        self.b.pack()
+        self.label=Label(top,text="Hello World")
+        self.label.pack()
+        self.entry=Entry(top)
+        self.entry.pack()
+        self.button=Button(top,text='Ok',command=self.cleanup)
+        self.button.pack()
     def cleanup(self):
-        self.value=self.e.get()
+        self.value=self.entry.get()
         self.top.destroy()
 
 class mainWindow(object):
     def __init__(self,master):
         self.master=master
-        self.b=Button(master,text="click me!",command=self.popup)
-        self.b.pack()
-        self.b2=Button(master,text="print value",command=lambda: sys.stdout.write(self.entryValue()+'\n'))
-        self.b2.pack()
+        self.button=Button(master,text="click me!",command=self.popup)
+        self.button.pack()
+        self.button2=Button(master,text="print value",command=lambda: sys.stdout.write(self.entryValue()+'\n'))
+        self.button2.pack()
 
     def popup(self):
-        self.w=popupWindow(self.master)
-        self.b["state"] = "disabled" 
-        self.master.wait_window(self.w.top)
-        self.b["state"] = "normal"
+        self.window=popupWindow(self.master)
+        self.button["state"] = "disabled"
+        self.master.wait_window(self.window.top)
+        self.button["state"] = "normal"
 
     def entryValue(self):
-        return self.w.value
+        return self.window.value
 
 
 if __name__ == "__main__":
